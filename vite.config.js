b2974cbf476536e2e99dd9286d7e0d2b55e0c793
exslym <76844562+exslym@users.noreply.github.com>
2024-12-05 12:59:12 +0100

    file structure updated for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	root: './src',
+	publicDir: '../public',
+	base: './',
+
 	build: {
 		outDir: './build',
 		emptyOutDir: true,
